4b9082753f753aa7716ee0bafda1037f3abe7def
RinAMFT <mucurkaarina@gmail.com>
2025-02-11 11:04:08 +0200

    Add files via upload

---
+++ ConsoleApp23/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n=");
+            int n = int.Parse(Console.ReadLine());
+            int[] omas = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"mas[{i}]=");
+                omas[i] = int.Parse(Console.ReadLine());
+                Console.WriteLine($"mas[{i}]={omas[i]}");
+            }
+            //Console.Write("n=");
+            //int n= int.Parse(Console.ReadLine());
+            //int[] omas = new int[n];
+            //for (int i = 0; i< n; i++)
+            //{
+            //    Console.Write($"mas[{i}]=");
+            //    omas[i] = int.Parse(Console.ReadLine());
+            //}
+            //for(int i = 0;i< n; i++)
+            //{
+            //    Console.WriteLine($"mas[{i}]={omas[i]}");
+            //}
+            Console.ReadKey();
+        }
+    }
+}
