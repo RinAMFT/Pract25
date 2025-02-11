9623d2d112460819c4edb9049b5ab123dfc98e31
RinAMFT <mucurkaarina@gmail.com>
2025-02-11 11:09:27 +0200

    Add files via upload

---
+++ ConsoleApp24/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] omas = { 3, -3, 5, 6, -7, 8 };
+            //int[]omas=now int[] {3,-3,5,6,-7,8}
+            foreach(var element in omas)
+            {
+                Console.WriteLine($"{element}\t");
+            }
+            Console.ReadKey();
+        }
+    }
+}
