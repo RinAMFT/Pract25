d9fa2ac67235c8900f7d131654c59587422350b9
RinAMFT <mucurkaarina@gmail.com>
2025-02-17 22:28:07 +0200

    Add files via upload

---
+++ ConsoleApp26/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int size = 10; 
+            int[] mas = new int[size];
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas[i] = i;
+            }
+            foreach (int element in mas)
+            {
+                Console.WriteLine(element);
+            }
+            Console.ReadKey();  
+        }
+    }
+}
